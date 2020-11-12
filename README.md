9f07454ce5e66c339b47af123df7f1486a2220cf
labimas <labimas@lnls.br>
2020-11-12 13:36:08 -0300

    Adding installation file

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # helmholtz-coil-control
-Graphical user interface for helmholtz coil control
+Graphical user interface for Helmholtz coil control
+
+## Installation
+
+This package can be installed using setuptools.
+
+To install a development version, with a link to the local package files in the
+default Python directory, run
+
+    <python> setup.py develop
+
+To install to a default Python directory, run
+
+    <python> setup.py install
+
+Use the appropriate command in <python>. Depending on permissions, it may be
+necessary to run the command with sudo or as superuser.
+
+Details and further options can be found in setuptools documentation.
---
+++ setup.py
@@ -0,0 +1,30 @@
+
+import os
+from setuptools import setup
+
+
+basedir = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(basedir, 'VERSION'), 'r') as _f:
+    __version__ = _f.read().strip()
+
+
+setup(
+    name='helmholtz',
+    version=__version__,
+    description='Helmholtz coil control application',
+    url='https://github.com/lnls-ima/helmholtz-coil-control',
+    author='lnls-ima',
+    license='MIT License',
+    packages=['helmholtz'],
+    install_requires=[
+        'pyvisa',
+        'numpy',
+        'scipy',
+        'pandas',
+        'pyqtgraph',
+        'pyserial',
+        'qtpy',
+    ],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    zip_safe=False)
