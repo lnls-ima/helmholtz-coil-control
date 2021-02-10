59bf99fe5841865a75f7d606b123613953d03bbf
Luana Vilela <luana.vilela@lnls.br>
2021-02-10 06:11:35 -0300

    Adding tabs in results dialog

--- helmholtz/gui/resultsdialog.py
+++ helmholtz/gui/resultsdialog.py
@@ -21,11 +21,15 @@ from qtpy.QtGui import QFont as _QFont
 from qtpy.QtCore import (
     Qt as _Qt,
     QCoreApplication as _QCoreApplication,
-    )   
+    )
 import qtpy.uic as _uic
 import pyqtgraph as _pyqtgraph
 
+import helmholtz.data as _data
 from helmholtz.gui import utils as _utils
+from helmholtz.gui.auxiliarywidgets import (
+    CheckableComboBox as _CheckableComboBox,
+    )
 
 
 class ResultsDialog(_QDialog):
@@ -40,6 +44,7 @@ class ResultsDialog(_QDialog):
 
         self.measurement_list = []
         self.measurement_idns = []
+        self.integration_trigger_list = []
         self.mx = []
         self.my = []
         self.mz = []
@@ -49,13 +54,32 @@ class ResultsDialog(_QDialog):
         self.xmin_line = None
         self.xmax_line = None
 
-        # Create legend
-        self.legend = _pyqtgraph.LegendItem(offset=(70, 30))
-        self.legend.setParentItem(self.ui.pw_graph.graphicsItem())
-        self.legend.setAutoFillBackground(1)
-        self.legend_items = []
+        # Create legends
+        self.legend_mag = _pyqtgraph.LegendItem(offset=(70, 30))
+        self.legend_mag.setParentItem(self.ui.pw_graph_mag.graphicsItem())
+        self.legend_mag.setAutoFillBackground(1)
+        self.legend_mag_items = []
+
+        # Add combo box
+        self.cmb_idns = _CheckableComboBox()
+        self.ui.hlt_select_ids.addWidget(self.cmb_idns)
 
         self.connect_signal_slots()
+
+    @property
+    def database_name(self):
+        """Database name."""
+        return _QApplication.instance().database_name
+
+    @property
+    def mongo(self):
+        """MongoDB database."""
+        return _QApplication.instance().mongo
+
+    @property
+    def server(self):
+        """Server for MongoDB database."""
+        return _QApplication.instance().server
 
     def accept(self):
         """Close dialog."""
@@ -74,7 +98,7 @@ class ResultsDialog(_QDialog):
     def calc_curve_fit(self):
         """Calculate curve fit."""
         try:
-            plots = self.get_selected_plots()
+            plots = self.get_selected_plots_mag()
             if len(plots) == 0:
                 return
 
@@ -105,8 +129,8 @@ class ResultsDialog(_QDialog):
                     rcount, 1, _QTableWidgetItem(str(value)))
                 rcount = rcount + 1
 
-            self.update_plot()
-            self.ui.pw_graph.plotItem.plot(xfit, yfit, pen=(0, 0, 0))
+            self.update_plot_mag()
+            self.ui.pw_graph_mag.plotItem.plot(xfit, yfit, pen=(0, 0, 0))
 
         except Exception:
             _traceback.print_exc(file=_sys.stdout)
@@ -115,13 +139,15 @@ class ResultsDialog(_QDialog):
         """Clear all."""
         self.measurement_list = []
         self.measurement_idns = []
+        self.integration_trigger_list = []
         self.mx = []
         self.my = []
         self.mz = []
         self.xmin_line = None
         self.xmax_line = None
         self.clear_fit()
-        self.clear_graph()
+        self.cmb_idns.clear()
+        self.clear_plots()
 
     def clear_fit(self):
         """Clear fit."""
@@ -129,20 +155,41 @@ class ResultsDialog(_QDialog):
         self.ui.tbl_fit.setRowCount(0)
         self.ui.la_fitfunction.setText('')
 
-    def clear_graph(self):
-        """Clear plots."""
-        for item in self.legend_items:
-            self.legend.removeItem(item)
-        self.legend_items = []
-        self.ui.pw_graph.plotItem.curves.clear()
-        self.ui.pw_graph.clear()
+    def clear_plots(self):
+        self.clear_plot_mag()
+        self.clear_plot_iv()
+        self.clear_plot_vol_temp()
+
+    def clear_plot_mag(self):
+        """Clear magnetization plot."""
+        for item in self.legend_mag_items:
+            self.legend_mag.removeItem(item)
+        self.legend_mag_items = []
+        self.ui.pw_graph_mag.plotItem.curves.clear()
+        self.ui.pw_graph_mag.clear()
         self.graph_mx = None
         self.graph_my = None
         self.graph_mz = None
 
-    def configure_graph(self):
-        """Configure graph."""
-        self.clear_graph()
+    def clear_plot_iv(self):
+        """Clear integrated voltage plots."""
+        pass
+
+    def clear_plot_vol_temp(self):
+        """Clear volume and temperature plots."""
+        pass
+
+    def configure_plot_iv(self):
+        """Configure integrated voltage plot."""
+        self.clear_plot_iv()
+
+    def configure_plot_vol_temp(self):
+        """Configure integrated voltage plot."""
+        self.clear_plot_vol_temp()
+
+    def configure_plot_mag(self):
+        """Configure magnetization plot."""
+        self.clear_plot_mag()
 
         color_mx = (255, 0, 0)
         color_my = (0, 255, 0)
@@ -153,10 +200,10 @@ class ResultsDialog(_QDialog):
         label_style = {'font-size': font_str}
         font = _QFont()
         font.setPixelSize(_utils.PLOT_FONT_SIZE)
-        
+
         symbol_size = 8
         pen = _pyqtgraph.mkPen(color=color_mx, width=width)
-        self.graph_mx = self.ui.pw_graph.plotItem.plot(
+        self.graph_mx = self.ui.pw_graph_mag.plotItem.plot(
             _np.array([]),
             _np.array([]),
             symbol='o',
@@ -164,9 +211,9 @@ class ResultsDialog(_QDialog):
             symbolSize=symbol_size,
             symbolBrush=color_mx)
         self.graph_mx.setPen(pen)
-                           
+
         pen = _pyqtgraph.mkPen(color=color_my, width=width)
-        self.graph_my = self.ui.pw_graph.plotItem.plot(
+        self.graph_my = self.ui.pw_graph_mag.plotItem.plot(
             _np.array([]),
             _np.array([]),
             symbol='o',
@@ -176,7 +223,7 @@ class ResultsDialog(_QDialog):
         self.graph_my.setPen(pen)
 
         pen = _pyqtgraph.mkPen(color=color_mz, width=width)
-        self.graph_mz = self.ui.pw_graph.plotItem.plot(
+        self.graph_mz = self.ui.pw_graph_mag.plotItem.plot(
             _np.array([]),
             _np.array([]),
             symbol='o',
@@ -185,43 +232,75 @@ class ResultsDialog(_QDialog):
             symbolBrush=color_mz)
         self.graph_mz.setPen(pen)
 
-        self.legend_items = ['Mx', 'My', 'Mz']
-        self.legend.addItem(self.graph_mx, self.legend_items[0])
-        self.legend.addItem(self.graph_my, self.legend_items[1])
-        self.legend.addItem(self.graph_mz, self.legend_items[2])
+        self.legend_mag_items = ['Mx', 'My', 'Mz']
+        self.legend_mag.addItem(self.graph_mx, self.legend_mag_items[0])
+        self.legend_mag.addItem(self.graph_my, self.legend_mag_items[1])
+        self.legend_mag.addItem(self.graph_mz, self.legend_mag_items[2])
 
-        self.ui.pw_graph.setLabel(
+        self.ui.pw_graph_mag.setLabel(
             'left', text='Magnetization [T]', **label_style)
 
-        self.ui.pw_graph.getAxis('left').tickFont = font
-        self.ui.pw_graph.getAxis('left').setStyle(
+        self.ui.pw_graph_mag.getAxis('left').tickFont = font
+        self.ui.pw_graph_mag.getAxis('left').setStyle(
             tickTextOffset=_utils.PLOT_FONT_SIZE)
 
-        self.ui.pw_graph.setLabel(
-            'bottom', text="Database ID", **label_style)
-        self.ui.pw_graph.getAxis('bottom').tickFont = font
-        self.ui.pw_graph.getAxis('bottom').setStyle(
+        xlabel = self.get_bottom_axis_label()
+        self.ui.pw_graph_mag.setLabel(
+            'bottom', text=xlabel, **label_style)
+        self.ui.pw_graph_mag.getAxis('bottom').tickFont = font
+        self.ui.pw_graph_mag.getAxis('bottom').setStyle(
             tickTextOffset=_utils.PLOT_FONT_SIZE)
 
-        self.ui.pw_graph.showGrid(x=True, y=True)
+        self.ui.pw_graph_mag.showGrid(x=True, y=True)
 
     def connect_signal_slots(self):
         """Create signal/slot connections."""
-        self.ui.pbt_update_plot.clicked.connect(self.update_plot)
-        self.ui.chb_mx.stateChanged.connect(self.update_controls_and_plot)
-        self.ui.chb_my.stateChanged.connect(self.update_controls_and_plot)
-        self.ui.chb_mz.stateChanged.connect(self.update_controls_and_plot)
+        self.ui.pbt_update_plot_mag.clicked.connect(self.update_plot_mag)
+        self.ui.chb_mx.stateChanged.connect(
+            self.enable_limits_and_update_plot_mag)
+        self.ui.chb_my.stateChanged.connect(
+            self.enable_limits_and_update_plot_mag)
+        self.ui.chb_mz.stateChanged.connect(
+            self.enable_limits_and_update_plot_mag)
         self.ui.cmb_fitfunction.currentIndexChanged.connect(
             self.fit_function_changed)
         self.ui.pbt_fit.clicked.connect(self.calc_curve_fit)
         self.ui.pbt_resetlim.clicked.connect(self.reset_xlimits)
+
+        self.ui.pbt_select_all.clicked.connect(
+            lambda: self.set_selection_all(True))
+        self.ui.pbt_clear_all.clicked.connect(
+            lambda: self.set_selection_all(False))
+        self.ui.pbt_update_plot_iv.clicked.connect(
+            self.update_plot_iv)
+
+        self.ui.pbt_update_plot_vol_temp.clicked.connect(
+            self.update_plot_vol_temp)
+
+        self.ui.cmb_bottom_axis.currentIndexChanged.connect(
+            self.update_plots)
+
+    def set_selection_all(self, checked):
+        """Set selection all."""
+        self.blockSignals(True)
+        if checked:
+            state = _Qt.Checked
+        else:
+            state = _Qt.Unchecked
+
+        for idx in range(self.cmb_idns.count()):
+            item = self.cmb_idns.model().item(idx, 0)
+            item.setCheckState(state)
+
+        self.blockSignals(False)
+        self.update_plot_iv()
 
     def update_xlimits(self):
         x = self.get_bottom_axis_data()
         self.ui.sbd_xmin.setValue(_np.min(x))
         self.ui.sbd_xmax.setValue(_np.max(x))
 
-    def get_selected_plots(self):
+    def get_selected_plots_mag(self):
         """Get all selected scans and components."""
         try:
             plots = []
@@ -238,12 +317,25 @@ class ResultsDialog(_QDialog):
             return None
 
     def get_bottom_axis_data(self):
-        return self.measurement_idns
+        text = self.ui.cmb_bottom_axis.currentText().lower()
+        data = self.measurement_idns
+        if text in ('integration trigger',):
+            if len(self.integration_trigger_list) > 0:
+                data = self.integration_trigger_list
+        return data
+
+    def get_bottom_axis_label(self):
+        text = self.ui.cmb_bottom_axis.currentText().lower()
+        label = 'Database ID'
+        if text in ('integration trigger',):
+            if len(self.integration_trigger_list) > 0:
+                label = 'Integration Trigger'
+        return label
 
     def fit_function_changed(self):
         """Hide or show polynomial fitting order and update dict value."""
         self.clear_fit()
-        self.update_plot()
+        self.update_plot_mag()
         func = self.ui.cmb_fitfunction.currentText()
         if func.lower() in ('polynomial', 'polinomial'):
             self.ui.la_polyorder.show()
@@ -255,7 +347,7 @@ class ResultsDialog(_QDialog):
     def get_x_y(self):
         """Get X, Y values."""
         xdata = self.get_bottom_axis_data()
-        plots = self.get_selected_plots()
+        plots = self.get_selected_plots_mag()
 
         if len(plots) == 0:
             return [], []
@@ -267,8 +359,9 @@ class ResultsDialog(_QDialog):
         elif 'mz' in plots:
             ydata = self.mz
 
-        x = _np.array(xdata)
-        y = _np.array(ydata)
+        sorted_data = _np.array(sorted(zip(xdata, ydata)))
+        x = sorted_data[:, 0]
+        y = sorted_data[:, 1]
 
         xmin = self.ui.sbd_xmin.value()
         xmax = self.ui.sbd_xmax.value()
@@ -290,7 +383,7 @@ class ResultsDialog(_QDialog):
 
         self.ui.sbd_xmin.setValue(xmin)
         self.ui.sbd_xmax.setValue(xmax)
-        self.update_plot()
+        self.update_plot_mag()
 
     def show(self, measurement_list):
         """Update data and show dialog."""
@@ -304,6 +397,26 @@ class ResultsDialog(_QDialog):
             self.measurement_list = measurement_list
             self.measurement_idns = [m.idn for m in measurement_list]
 
+            for index, idn in enumerate(self.measurement_idns):
+                self.cmb_idns.addItem(str(idn))
+                item = self.cmb_idns.model().item(index, 0)
+                item.setCheckState(_Qt.Checked)
+
+            self.integration_trigger_list = []
+            for m in self.measurement_list:
+                adv_opt_idn = m.advanced_options_id
+                adv_opt = _data.configuration.AdvancedOptions(
+                    database_name=self.database_name,
+                    mongo=self.mongo, server=self.server)
+                adv_opt.db_read(adv_opt_idn)
+
+                trigger = adv_opt.integration_trigger
+                if trigger is None:
+                    self.integration_trigger_list = []
+                    break
+
+                self.integration_trigger_list.append(trigger)
+
             self.mx = []
             self.my = []
             self.mz = []
@@ -314,8 +427,9 @@ class ResultsDialog(_QDialog):
 
             self.ui.la_polyorder.hide()
             self.ui.sb_polyorder.hide()
+            self.cmb_idns.setCurrentIndex(-1)
             self.reset_xlimits()
-            self.update_controls_and_plot()
+            self.enable_limits_and_update_plot_mag()
             super().show()
 
         except Exception:
@@ -325,11 +439,11 @@ class ResultsDialog(_QDialog):
             _QMessageBox.critical(self, 'Failure', msg, _QMessageBox.Ok)
             return
 
-    def update_controls(self):
+    def enable_limits(self):
         """Enable or disable group boxes."""
         self.clear_fit()
 
-        plots = self.get_selected_plots()
+        plots = self.get_selected_plots_mag()
         if len(plots) == 1:
             self.ui.gb_xlimits.setEnabled(True)
             self.ui.gb_fit.setEnabled(True)
@@ -337,17 +451,22 @@ class ResultsDialog(_QDialog):
             self.ui.gb_xlimits.setEnabled(False)
             self.ui.gb_fit.setEnabled(False)
 
-    def update_controls_and_plot(self):
+    def enable_limits_and_update_plot_mag(self):
         """Update controls and plot."""
-        self.update_controls()
-        self.update_plot()
+        self.enable_limits()
+        self.update_plot_mag()
 
-    def update_plot(self):
-        """Update plot."""
+    def update_plots(self):
+        self.update_plot_mag()
+        self.update_plot_iv()
+        self.update_plot_vol_temp()
+
+    def update_plot_mag(self):
+        """Update magnetization plot."""
         try:
-            self.clear_graph()
+            self.clear_plot_mag()
 
-            plots = self.get_selected_plots()
+            plots = self.get_selected_plots_mag()
 
             if len(plots) == 0:
                 show_xlines = False
@@ -358,18 +477,27 @@ class ResultsDialog(_QDialog):
             else:
                 show_xlines = True
 
-            self.configure_graph()
+            self.configure_plot_mag()
 
             with _warnings.catch_warnings():
                 _warnings.simplefilter("ignore")
                 x = self.get_bottom_axis_data()
 
                 if 'mx' in plots:
-                    self.graph_mx.setData(x, self.mx)
+                    sorted_data = _np.array(sorted(zip(x, self.mx)))
+                    x_sorted = sorted_data[:, 0]
+                    mx_sorted = sorted_data[:, 1]
+                    self.graph_mx.setData(x_sorted, mx_sorted)
                 if 'my' in plots:
-                    self.graph_my.setData(x, self.my)
+                    sorted_data = _np.array(sorted(zip(x, self.my)))
+                    x_sorted = sorted_data[:, 0]
+                    my_sorted = sorted_data[:, 1]
+                    self.graph_my.setData(x_sorted, my_sorted)
                 if 'mz' in plots:
-                    self.graph_mz.setData(x, self.mz)
+                    sorted_data = _np.array(sorted(zip(x, self.mz)))
+                    x_sorted = sorted_data[:, 0]
+                    mz_sorted = sorted_data[:, 1]
+                    self.graph_mz.setData(x_sorted, mz_sorted)
 
             if show_xlines:
                 xmin = self.ui.sbd_xmin.value()
@@ -377,13 +505,13 @@ class ResultsDialog(_QDialog):
 
                 self.xmin_line = _pyqtgraph.InfiniteLine(
                     xmin, pen=(0, 0, 0), movable=True)
-                self.ui.pw_graph.addItem(self.xmin_line)
+                self.ui.pw_graph_mag.addItem(self.xmin_line)
                 self.xmin_line.sigPositionChangeFinished.connect(
                     self.update_xmin_spin_box)
 
                 self.xmax_line = _pyqtgraph.InfiniteLine(
                     xmax, pen=(0, 0, 0), movable=True)
-                self.ui.pw_graph.addItem(self.xmax_line)
+                self.ui.pw_graph_mag.addItem(self.xmax_line)
                 self.xmax_line.sigPositionChangeFinished.connect(
                     self.update_xmax_spin_box)
             else:
@@ -393,6 +521,12 @@ class ResultsDialog(_QDialog):
         except Exception:
             _traceback.print_exc(file=_sys.stdout)
             return
+
+    def update_plot_iv(self):
+        pass
+
+    def update_plot_vol_temp(self):
+        pass
 
     def update_xmax_spin_box(self):
         """Update xmax value."""
